7bcc7b4d97f36e3820811626d4f5df9629bb070e
RafaelMachado99 <rafael.894@hotmail.com>
2023-09-18 14:19:05 -0300

    Criando a main

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+//RAFAEL
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        AcquisitionSystem system = new AcquisitionSystem();
+        // ACABA RAFAEL
+
+    }
+}
