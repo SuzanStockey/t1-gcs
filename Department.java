fe6db24dc6d2b91b9de1d9c31a1f2f689d0908ea
crlnslvr <csilveira2612@gmail.com>
2023-09-24 16:49:37 -0300

    Adicionando o arquivo Department.java da classe Department

---
+++ Department.java
@@ -0,0 +1,9 @@
+public class Department {
+    String name;
+    double maxRequestAmount;
+
+    public Department(String name, double maxRequestAmount) {
+        this.name = name;
+        this.maxRequestAmount = maxRequestAmount;
+    }
+}
