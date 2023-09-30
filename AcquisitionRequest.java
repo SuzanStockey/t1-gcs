0a0ea8d44ff218898f2690e8303e42cf76cbf0c2
Lucas Bianchessi <l.bianchessi@edu.pucrs.br>
2023-09-30 17:37:51 -0300

    AcquisitionResquest.java foi adicionado as variaveis de instancia e metodo consstrutor

--- AcquisitionRequest.java
+++ AcquisitionRequest.java
@@ -1,5 +1,21 @@
-public class AcquisitionRequest{
-	public static void main(String[] args){
-		//por enquanto nada
-	}
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AcquisitionRequest {
+    int id;
+    User solicitante;
+    Department departamento_solicitante;
+    Date data_pedido;
+    Date data_termino;
+    RequestStatus status;
+    List<RequestItem> itens = new ArrayList<>();
+
+    public AcquisitionRequest(int id, User solicitante) {
+        this.id = id;
+        this.solicitante = solicitante;
+        departamento = solicitante.getDepartment();
+        data_pedido = new Date();
+        status = RequestStatus.OPEN;
+    }
 }
