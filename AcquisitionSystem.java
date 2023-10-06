8a3c0eb651303c171e717b22740d0424f19d549e
Adrielle Dornelles Dewes <adrielle.dewes@edu.pucrs.br>
2023-10-06 16:13:36 -0300

    implementação do método evaluateRequest

--- AcquisitionSystem.java
+++ AcquisitionSystem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
 public class AcquisitionSystem {
     List<Department> departments = new ArrayList<>();
     List<User> users = new ArrayList<>();
@@ -20,5 +21,29 @@ public class AcquisitionSystem {
     public void setCurrentUser(User user) {
         currentUser = user;
     }
-    
+    public void evaluateRequest(User evaluator, AcquisitionRequest request, RequestStatus status) {
+        if (evaluator == null || request == null || status == null) {
+            System.out.println("Invalid evaluation.");
+            return;
+        }
+
+        if (evaluator.type != UserType.ADMINISTRATOR) {
+            System.out.println("Only administrators can evaluate requests.");
+            return;
+        }
+
+        if (request.status.equals(RequestStatus.OPEN)) {
+            System.out.println("Request has already been evaluated.");
+            return;
+        }
+
+        request.status = status;
+
+        if (status == RequestStatus.APPROVED) {
+            System.out.println("Request approved.");
+        } else if (status == RequestStatus.REJECTED) {
+            System.out.println("Request rejected.");
+        }
+    }
+
 }
