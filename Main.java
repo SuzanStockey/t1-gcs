319bf14fdf8a23569d2ec6c7114d36e6dbfc3e7f
RafaelMachado99 <rafael.894@hotmail.com>
2023-10-16 15:26:25 -0300

    atualizando main

--- Main.java
+++ Main.java
@@ -10,13 +10,37 @@ public class Main {
         AcquisitionSystem system = new AcquisitionSystem();
         // ACABA RAFAEL
 
-        //Carol
+        // Carol
         Department finance = new Department("Finance", 1000);
         Department rh = new Department("RH", 800);
         Department engineering = new Department("Engineering", 1500);
         Department maintenance = new Department("Manutenção", 1200);
         Department diretoria = new Department("Diretoria", 900);
         // Acaba Carol
+
+        // RAFAEL
+        system.addDepartment(finance);
+        system.addDepartment(rh);
+        system.addDepartment(engineering);
+        system.addDepartment(maintenance);
+        system.addDepartment(diretoria);
+
+        system.addUser(admin);
+        system.addUser(employee1);
+        system.addUser(employee2);
+        system.addUser(employee3);
+        system.addUser(employee4);
+        system.addUser(employee5);
+        system.addUser(employee6);
+        system.addUser(employee7);
+        system.addUser(employee8);
+        system.addUser(employee9);
+        system.addUser(employee10);
+        system.addUser(employee11);
+        system.addUser(employee12);
+        system.addUser(employee13);
+        system.addUser(employee14);
+        // ACABA RAFAEL
 
     }
 }
